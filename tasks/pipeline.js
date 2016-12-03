360826695ed8291bb84a9b1caa6f31ce6c8b31f0
pantsel <tselentispanagis@gmail.com>
2016-12-03 04:16:49 +0200

    finalizing

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -48,7 +48,6 @@ var jsFilesToInject = [
   '/bower_components/angular-simple-logger/dist/angular-simple-logger.js',
   '/bower_components/angular-google-maps/dist/angular-google-maps.js',
   '/bower_components/angular-ui-router/release/angular-ui-router.js',
-  '/bower_components/tinymce-dist/tinymce.js',
 
   'js/main.js',
 
